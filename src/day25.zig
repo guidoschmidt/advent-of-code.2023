10c4000c569e77d3ed0faef58cbdf87ad18a757e
Guido Schmidt <git@guidoschmidt.cc>
2024-05-24 09:49:53 +0200

    WIP/day25.zig

--- src/day25.zig
+++ src/day25.zig
@@ -3,107 +3,248 @@ const common = @import("common.zig");
 
 const Allocator = std.mem.Allocator;
 
-const Link = struct {
-    a: []const u8,
-    b: []const u8,
+const Node = struct {
+    name: []const u8,
+    links: std.ArrayList(?*Node),
+
+    pub fn deinit(self: *Node) void {
+        self.links.deinit();
+    }
+
+    pub fn format(self: *Node,
+                  comptime fmt: []const u8,
+                  options: std.fmt.FormatOptions,
+                  writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+        try writer.print("{s} ", .{ self.name });
+    }
+
 };
 
-fn belongsToGroup(group_map: *std.AutoHashMap(u8, std.BufSet), comp: []const u8) ?u8 {
-    var belongs_to_group: ?u8 = null;
-    var group_map_it = group_map.*.iterator();
-    while(group_map_it.next()) |group| {
-        var group_items_it = group.value_ptr.*.iterator();
-        while (group_items_it.next()) |group_item| {
-            if (std.mem.eql(u8, group_item.*, comp)) {
-                belongs_to_group = group.key_ptr.*;
+const Graph = struct {
+    nodes: std.StringHashMap(Node),
+    allocator: Allocator,
+
+    pub fn getRandomNode(self: *Graph, rng: *std.rand.Xoshiro256) *Node {
+        var keys_it = self.nodes.keyIterator();
+
+        const idx_a = rng.random().intRangeAtMost(usize, 1, self.nodes.count() - 1);
+        var i : usize = 0;
+        while (i < idx_a) : (i += 1) {
+            _ = keys_it.next();
+        }
+        const key_a = keys_it.next().?.*;
+        return self.nodes.getPtr(key_a).?;
+    }
+
+    pub fn addNode(self: *Graph, name: []const u8) !void {
+        try self.nodes.put(name, Node {
+            .name = name,
+            .links = std.ArrayList(?*Node).init(self.allocator),
+        });
+    }
+
+    pub fn addLink(self: *Graph, from: []const u8, to: []const u8) !void {
+        if (self.nodes.contains(from) and self.nodes.contains(to)) {
+            const from_ptr = self.nodes.getPtr(from);
+            const to_ptr = self.nodes.getPtr(to);
+            if (from_ptr != null and to_ptr != null) {
+                try from_ptr.?.links.append(to_ptr.?);
             }
         }
     }
-    return belongs_to_group;
-}
 
-fn findGroups(gpa: Allocator, comp_conn_map: std.StringHashMap(std.ArrayList([]const u8))) !void {
-    var group_map = std.AutoHashMap(u8, std.BufSet).init(gpa);
-
-    var comp_map_it = comp_conn_map.iterator();
-    while (comp_map_it.next()) |e| {
-        const comp_name =  e.key_ptr.*;
-        if (group_map.count() == 0) {
-            var set = std.BufSet.init(gpa);
-            try set.insert(comp_name);
-            for (e.value_ptr.*.items) |conn| {
-                try set.insert(conn);
+    pub fn removeLink(self: *Graph, from: []const u8, to: []const u8) void {
+        if (self.nodes.getPtr(from)) |node| {
+            var found_idx: ?usize =  null;
+            for(0..node.links.items.len) |i| {
+                if (std.mem.eql(u8, node.links.items[i].name, to)) {
+                    found_idx = i;
+                    break;
+                }
             }
-            try group_map.put(0, set);
-            continue;
+            if (found_idx != null)
+                _ = node.links.swapRemove(found_idx.?);
         }
-
-        var group_id: ?u8 = null;
-        for (e.value_ptr.*.items) |conn| {
-            group_id = belongsToGroup(&group_map, conn);
-            if (group_id != null) break;
-        }
-        if (group_id != null) {
-            var group_components = group_map.get(group_id.?).?;
-            try group_components.insert(comp_name);
-            try group_map.put(group_id.?, group_components);
-        } else {
-            var new_set = std.BufSet.init(gpa);
-            try new_set.insert(comp_name);
-            try group_map.put(@intCast(group_map.count()), new_set);
+        if (self.nodes.getPtr(to)) |node| {
+            var found_idx: ?usize = null;
+            for(0..node.links.items.len) |i| {
+                if (std.mem.eql(u8, node.links.items[i].name, from)) {
+                    found_idx = i;
+                    break;
+                }
+            }
+            if (found_idx != null)
+                _ = node.links.swapRemove(found_idx.?);
         }
     }
 
-    for(0..group_map.count()) |g_id| {
-        const g = group_map.get(@intCast(g_id)).?;
-        std.debug.print("\n\nGroup {d} [{d}]\n    ", .{ g_id, g.count() });
-        var group_comp_it = g.iterator();
-        while (group_comp_it.next()) |c| {
-            std.debug.print("{s},", .{ c.* });
+    pub fn contractLink(self: *Graph, from: []const u8, to: []const u8) !*Node {
+        const contracted_node_name = try std.fmt.allocPrint(self.allocator, "{s}-{s}", .{ from, to });
+        var new_node = Node {
+            .name = contracted_node_name,
+            .links = std.ArrayList(?*Node).init(self.allocator),
+        };
+        try self.nodes.put(contracted_node_name, new_node);
+        const new_node_ptr = self.nodes.getPtr(contracted_node_name).?;
+
+        const combinations = .{ .{ from, to }, .{ to, from } };
+        inline for (0..combinations.len) |i| {
+            const combination = combinations[i];
+            const a = combination[0];
+            const b = combination[1];
+            std.debug.print("\n{s} x {s}", .{ a, b });
+            // Update links
+            if (self.nodes.getPtr(a)) |node| {
+                for (node.links.items) |link| {
+                    if (!std.mem.eql(u8, link.?.name, b) and self.nodes.contains(link.?.*.name)) {
+                        std.debug.print("\nCollecting links: {s}", .{ link.?.*.name });
+                        try new_node.links.append(self.nodes.getPtr(link.?.name));
+                    }
+                }
+
+                var found_idx: ?usize = null;
+                for (0..node.links.items.len) |k| {
+                    if (std.mem.eql(u8, node.links.items[k].?.name, b)) {
+                        found_idx = k;
+                        break;
+                    }
+                }
+                if (found_idx) |remove_at| {
+                    _ = node.links.swapRemove(remove_at);
+                }
+            }
+            var node_key_it = self.nodes.keyIterator();
+            while (node_key_it.next()) |key| {
+                if (self.nodes.getPtr(key.*)) |node| {
+                    var found_idx: ?usize = null;
+                    for (0..node.links.items.len) |r| {
+                        if (std.mem.eql(u8, a, node.links.items[r].?.name)) {
+                            found_idx = r;
+                        }
+                    }
+                    if (found_idx) |remove_at| {
+                        _ = node.links.swapRemove(remove_at);
+                        try node.links.append(new_node_ptr);
+                    }
+                }
+            }
+            // Remove node
+            if (self.nodes.getPtr(a)) |node| {
+                node.deinit();
+                _ = self.nodes.remove(a);
+            }
+        }
+
+        return new_node_ptr;
+    }
+
+    pub fn format(self: Graph,
+                  comptime fmt: []const u8,
+                  options: std.fmt.FormatOptions,
+                  writer: anytype) !void {
+        _ = fmt;
+        _ = options;
+        var node_it = self.nodes.iterator();
+        try writer.print("\nGRAPH--- [{d}]", .{ self.nodes.count() });
+        while (node_it.next()) |node| {
+            try writer.print("\n{s}", .{ node.key_ptr.* });
+            for (node.value_ptr.links.items) |link| {
+                try writer.print("\n -- {any}", .{ link });
+            }
         }
     }
-}
 
-fn part1(gpa: Allocator, input: []const u8) anyerror!void {
-    var component_connection_map = std.StringHashMap(std.ArrayList([]const u8)).init(gpa);
+};
 
+fn part1(allocator: Allocator, input: []const u8) anyerror!void {
+    var graph = Graph {
+        .nodes = std.StringHashMap(Node).init(allocator),
+        .allocator = allocator,
+    };
+
+    // Collect component names
     var row_it = std.mem.tokenizeAny(u8, input, "\n");
     while(row_it.next()) |row| {
         var config_it = std.mem.tokenizeAny(u8, row, ":");
-        var component_str = config_it.next().?;
-        var connections_it = std.mem.tokenizeAny(u8, config_it.next().?, " ");
-        var local_connections_list = std.ArrayList([]const u8).init(gpa);
-        while (connections_it.next()) |connection| {
-            try local_connections_list.append(connection);
-        }
+        const component = config_it.next().?;
+        try graph.addNode(component);
 
-        if (component_connection_map.get(component_str) == null) {
-            try component_connection_map.put(component_str, local_connections_list);
-            for (local_connections_list.items) |c| {
-                var other_list = std.ArrayList([]const u8).init(gpa);
-                try other_list.append(component_str);
-                try component_connection_map.put(c, other_list);
-            }
+        var connections_it = std.mem.tokenizeAny(u8, config_it.next().?, " ");
+        while(connections_it.next()) |connection| {
+            try graph.addNode(connection);
+        }
+    }
+
+    // Collect links
+    row_it.reset();
+    while(row_it.next()) |row| {
+        var config_it = std.mem.tokenizeAny(u8, row, ":");
+        const component = config_it.next().?;
+
+        var connections_it = std.mem.tokenizeAny(u8, config_it.next().?, " ");
+        while(connections_it.next()) |connection| {
+            try graph.addLink(component, connection);
+        }
+    }
+    std.debug.print("\n{any}", .{ graph });
+
+    // graph.removeLink("hfx", "pzl");
+    // graph.removeLink("bvb", "cmg");
+    // graph.removeLink("nvd", "jqt");
+
+    var queque = std.ArrayList(*Node).init(allocator);
+
+    const rng_gen = std.rand.DefaultPrng;
+    var rng: std.rand.Xoshiro256 = rng_gen.init(0);
+    try queque.append(graph.getRandomNode(&rng));
+    
+
+    while(queque.items.len > 0) {
+        const next = queque.pop();
+        std.debug.print("\n... Contracting {s}", .{ next.name });
+
+
+        if (next.links.items.len == 0) {
+            try queque.append(graph.getRandomNode(&rng));
             continue;
         }
+        const idx_b = rng.random().intRangeAtMost(usize, 0, next.links.items.len - 1);
+        if (next.links.items[idx_b]) |node_b| {
+            std.debug.print("-- {s} [{d}]", .{ node_b.name, idx_b });
+            const new_node_ptr = try graph.contractLink(next.name, node_b.name);
+            if (new_node_ptr.links.items.len == 0) {
+                try queque.append(graph.getRandomNode(&rng));
+            } else {
+                try queque.append(new_node_ptr);
+            }
 
-        var prev_map_value = component_connection_map.get(component_str).?;
-        for (local_connections_list.items) |conn| {
-            try prev_map_value.append(conn);
+            std.debug.print("\n{any}", .{ graph });
+            common.blockAskForNext();
         }
-        try component_connection_map.put(component_str, prev_map_value);
     }
 
-    var map_it = component_connection_map.iterator();
-    while(map_it.next()) |e| {
-        std.debug.print("\n{s} [{d}]", .{ e.key_ptr.*, e.value_ptr.*.items.len,  });
-        for (e.value_ptr.*.items) |c| {
-            std.debug.print("\n    ⚈-⚈ {s}", .{ c });
+
+    std.debug.print("\n{any}", .{ graph });
+
+    // Cratea a GraphViz file for visualization
+    const file_path = "day25.dot";
+    const file = try std.fs.cwd().createFile(file_path, .{});
+    var fw = file.writer();
+
+    try fw.print("graph G {{", .{});
+    var it = graph.nodes.keyIterator();
+    while (it.next()) |key| {
+        if(graph.nodes.get(key.*)) |node| {
+            try fw.print("\n {s}", .{ node.name });
+            for (node.links.items) |link| {
+                if (link != null)
+                    try fw.print(" -- {s}", .{ link.?.name });
+            }
         }
     }
-    std.debug.print("\n{d} components", .{ component_connection_map.count() });
-
-    try findGroups(gpa, component_connection_map);
+    try fw.print("\n}}", .{});
 }
 
 fn part2(gpa: Allocator, input: []const u8) anyerror!void {
@@ -112,8 +253,10 @@ fn part2(gpa: Allocator, input: []const u8) anyerror!void {
 }
 
 pub fn main() !void {
-    var gpa_generator = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = gpa_generator.allocator();
+    // var gpa_generator = std.heap.GeneralPurposeAllocator(.{}){};
+    // const gpa = gpa_generator.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
 
-    try common.runPart(gpa, 25, .EXAMPLE, part1);
+    try common.runPart(arena.allocator(), 25, .EXAMPLE, part1);
 }
